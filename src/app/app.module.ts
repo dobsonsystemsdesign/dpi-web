5cc4c4b8d3aea5f960a8317e200a2809b8f34dce
jake <jddobson10@gmail.com>
2024-01-25 19:08:07 -0500

    Initial release Basic webpage with phone number, fb link and license number.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,7 +22,8 @@ import { ScrollStatusDirective } from './directives/scroll-status.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
